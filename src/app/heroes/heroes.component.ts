4fb35427f6931fb459deab3e99ca98dd3a700bef
Miguel Morais <miguelmoraisdev@gmail.com>
2022-11-20 16:55:51 -0300

    Adding Angular Materials

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero.model';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -8,10 +9,12 @@ import { Hero } from '../hero.model';
 })
 export class HeroesComponent implements OnInit{
 
-  hero: Hero = {
-    id: 1,
-    name: 'Wolverine'
-  };
+  heroes = HEROES;
+  selectedHero?: Hero;
+
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
 
   constructor() {}
 
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from "./hero.model";
+
+export const HEROES: Hero[] = [
+  {id: 1, name: 'Wolverine'},
+  {id: 2, name: 'Homem Aranha'},
+  {id: 3, name: 'Capitã Marvel'},
+  {id: 4, name: 'Hulk'},
+  {id: 5, name: 'Homem de Ferro'},
+  {id: 6, name: 'Pantera Negra'},
+  {id: 7, name: 'Batman'},
+  {id: 8, name: 'Super Homem'},
+  {id: 9, name: 'Flash'},
+  {id: 10, name: 'Magneto'},
+
+]
